ad0b9e5dd8e7b42c1eea4779686ad43bd9009af4
rhonabaun <baunrhonalyn1224@gmail.com>
2024-06-08 15:21:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
 function submitComment(){
   commentbox.textContent += comment.value.toString() + "\n"
+ comment.value=""
 }
 submit.addEventListener("click", submitComment) 
 function clickLike1() {
